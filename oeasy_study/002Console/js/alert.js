ed8e2eceb85eb7fd66378a7144bc0de2e243cdfd
sunshy <no.yangjun@gmail.com>
2018-05-18 18:37:01 +0800

    web

---
+++ oeasy_study/002Console/js/alert.js
@@ -0,0 +1,11 @@
+function taolu(){
+    alert("T1你猜一会会发生什么");
+    alert("T2再等等");
+    alert("T3也许会有奇迹")
+}
+alert("你怎么打开了这个网页");
+taolu()
+taolu()
+taolu()
+alert("我知道你有点好奇");
+alert("这是最后一句了，88")
